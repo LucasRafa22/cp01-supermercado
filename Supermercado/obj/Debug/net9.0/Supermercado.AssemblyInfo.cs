5a54e7c87657a6a49f9ece6d5eee10fb7ce5acc9
samyr <costosinho50@gmail.com>
2026-03-05 10:51:48 -0300

    Adicionando Pastas

--- Supermercado/obj/Debug/net9.0/Supermercado.AssemblyInfo.cs
+++ Supermercado/obj/Debug/net9.0/Supermercado.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermercado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+160e2aebca4bd3ddeafb6f9697ede20c82ecbde2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermercado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermercado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
