1b64ca0142370c890f2ad8e3615418029d23642d
LucasRafa22 <lucassolimene22@gmail.com>
2026-03-06 09:19:18 -0300

    feat: atributos na entidade categoria

--- Supermercado.Domain/Entities/Categoria.cs
+++ Supermercado.Domain/Entities/Categoria.cs
@@ -1,6 +1,32 @@
-namespace Supermercado.Domain.entities;
+using Supermercado.Domain.Commom;
 
-public class Categoria
+namespace Supermercado.Domain.Entities;
+
+public class Categoria : BaseEntity
 {
+    public string Nome { get; private set; }
+
+    public string Descricao { get; private set; } 
     
+    public Categoria(string nome, string descricao)
+    {
+        UpdateNome(nome);
+        UpdateDescricao(descricao);
+    }
+
+    public void UpdateNome(string nome)
+    {
+        if (string.IsNullOrWhiteSpace(nome))
+            throw new Exception("Nome da categoria não pode ser vazio.");
+
+        Nome = nome.Trim();
+    }
+
+    public void UpdateDescricao(string descricao)
+    {
+        if (string.IsNullOrWhiteSpace(descricao))
+            throw new Exception("Descrição inválida.");
+
+        Descricao = descricao.Trim();
+    }
 }
