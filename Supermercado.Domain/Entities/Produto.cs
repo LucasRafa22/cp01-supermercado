de47ce08eaf7c5e78ed0d71ab8aa32a35f733d83
LucasRafa22 <lucassolimene22@gmail.com>
2026-03-06 09:20:27 -0300

    feat: atributos na entidade produto

--- Supermercado.Domain/Entities/Produto.cs
+++ Supermercado.Domain/Entities/Produto.cs
@@ -1,6 +1,51 @@
-namespace Supermercado.Domain.entities;
+using Supermercado.Domain.Commom;
 
-public class Produto
+namespace Supermercado.Domain.Entities;
+
+public class Produto : BaseEntity
 {
-    
+    public string Nome { get; private set; }
+
+    public decimal Preco { get; private set; }
+
+    public int Estoque { get; private set; }
+
+    public Guid CategoriaId { get; private set; }
+
+    public Produto(
+        string nome,
+        decimal preco,
+        int estoque,
+        Guid categoriaId)
+    {
+        UpdateNome(nome);
+        UpdatePreco(preco);
+        UpdateEstoque(estoque);
+
+        CategoriaId = categoriaId;
+    }
+
+    public void UpdateNome(string nome)
+    {
+        if (string.IsNullOrWhiteSpace(nome))
+            throw new Exception("Nome do produto inválido.");
+
+        Nome = nome.Trim();
+    }
+
+    public void UpdatePreco(decimal preco)
+    {
+        if (preco <= 0)
+            throw new Exception("Preço deve ser maior que zero.");
+
+        Preco = preco;
+    }
+
+    public void UpdateEstoque(int estoque)
+    {
+        if (estoque < 0)
+            throw new Exception("Estoque inválido.");
+
+        Estoque = estoque;
+    }
 }
