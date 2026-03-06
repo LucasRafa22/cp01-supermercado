f79f4b5a4e02ca68203eb8e63cd267a17bc5a529
LucasRafa22 <lucassolimene22@gmail.com>
2026-03-06 09:21:19 -0300

    feat: atributos na entidade venda

--- Supermercado.Domain/Entities/Venda.cs
+++ Supermercado.Domain/Entities/Venda.cs
@@ -1,6 +1,27 @@
-namespace Supermercado.Domain.entities;
+using Supermercado.Domain.Commom;
 
-public class Venda
+namespace Supermercado.Domain.Entities;
+
+public class Venda : BaseEntity
 {
-    
+    public Guid ClienteId { get; private set; }
+
+    public DateTime DataVenda { get; private set; }
+
+    public decimal ValorTotal { get; private set; }
+
+    public Venda(Guid clienteId)
+    {
+        ClienteId = clienteId;
+        DataVenda = DateTime.Now;
+        ValorTotal = 0;
+    }
+
+    public void AtualizarValorTotal(decimal valor)
+    {
+        if (valor < 0)
+            throw new Exception("Valor inválido.");
+
+        ValorTotal = valor;
+    }
 }
