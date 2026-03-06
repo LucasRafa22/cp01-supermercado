05f1391d5c5a659ba14f4e043e7cb946fafd75de
LucasRafa22 <lucassolimene22@gmail.com>
2026-03-06 09:21:55 -0300

    feat: atributos na entidade itemVenda

--- Supermercado.Domain/Entities/itemVenda.cs
+++ Supermercado.Domain/Entities/itemVenda.cs
@@ -1,6 +1,32 @@
-namespace Supermercado.Domain.entities;
+using Supermercado.Domain.Commom;
 
-public class itemVenda
+namespace Supermercado.Domain.Entities;
+
+public class ItemVenda : BaseEntity
 {
-    
+    public Guid VendaId { get; private set; }
+
+    public Guid ProdutoId { get; private set; }
+
+    public int Quantidade { get; private set; }
+
+    public decimal PrecoUnitario { get; private set; }
+
+    public ItemVenda(
+        Guid vendaId,
+        Guid produtoId,
+        int quantidade,
+        decimal precoUnitario)
+    {
+        if (quantidade <= 0)
+            throw new Exception("Quantidade deve ser maior que zero.");
+
+        if (precoUnitario <= 0)
+            throw new Exception("Preço inválido.");
+
+        VendaId = vendaId;
+        ProdutoId = produtoId;
+        Quantidade = quantidade;
+        PrecoUnitario = precoUnitario;
+    }
 }
