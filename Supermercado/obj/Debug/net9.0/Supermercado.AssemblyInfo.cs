61257eeb5337e0418ae43554c2afebaf1b21f0e5
samyr <costosinho50@gmail.com>
2026-03-08 19:46:17 -0300

    feat: adicionando mer.png

--- Supermercado/obj/Debug/net9.0/Supermercado.AssemblyInfo.cs
+++ Supermercado/obj/Debug/net9.0/Supermercado.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermercado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2532f5c8024f388c44765aca19b2e2cfcce4d2bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05f1391d5c5a659ba14f4e043e7cb946fafd75de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermercado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermercado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
